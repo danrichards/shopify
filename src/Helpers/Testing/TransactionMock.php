7b31942146a48f09b8c9c255f5c4a9552de98175
Diogo Gomes <diogogomeswww@gmail.com>
2018-10-15 22:35:16 +0100

    TransactionMock

---
+++ src/Helpers/Testing/TransactionMock.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dan\Shopify\Helpers\Testing;
+
+use GuzzleHttp\Psr7\Response;
+
+class TransactionMock
+{
+    /**
+     * @param string $body
+     * @param int $statusCode
+     * @param array $headers
+     * @return Response
+     */
+    public static function create($body, $statusCode = 200, $headers = [])
+    {
+        return new Response($statusCode, $headers, $body);
+    }
+}
