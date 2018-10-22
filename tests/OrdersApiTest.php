6dc8ebc4aa92184550cfeabf8e0ac13525a8849f
Diogo Gomes <diogogomeswww@gmail.com>
2018-10-22 23:05:04 +0100

    post /admin/orders.json test api

--- tests/OrdersApiTest.php
+++ tests/OrdersApiTest.php
@@ -4,6 +4,7 @@ namespace Dan\Shopify\Test;
 
 use Dan\Shopify\Helpers\Testing\ModelFactory\OrderFactory;
 use Dan\Shopify\Helpers\Testing\TransactionMock;
+use Dan\Shopify\Models\Order;
 use PHPUnit\Framework\TestCase;
 
 class OrdersApiTest extends TestCase
@@ -51,5 +52,61 @@ class OrdersApiTest extends TestCase
         $this->assertEquals('GET', $api->lastRequestMethod());
         $this->assertEquals('/admin/orders/count.json', $api->lastRequestUri());
         $this->assertEquals(2, $response);
+    }
+
+    /**
+     * GET /admin/orders/123.json
+     * Retrieves a single order.
+     *
+     * @test
+     * @throws \Dan\Shopify\Exceptions\InvalidOrMissingEndpointException
+     * @throws \Dan\Shopify\Exceptions\ModelNotFoundException
+     * @throws \ReflectionException
+     */
+    public function it_gets_a_order()
+    {
+        $api = \Dan\Shopify\Shopify::fake([
+            TransactionMock::create(OrderFactory::create())
+        ]);
+
+        $response = $api->orders->find($order_id = 123);
+
+        $this->assertEquals(200, $api->lastResponseStatusCode());
+        $this->assertEquals(Order::class, get_class($response));
+        $this->assertEquals('GET', $api->lastRequestMethod());
+        $this->assertEquals('/admin/orders/123.json', $api->lastRequestUri());
+    }
+
+    /** /admin/orders/450789469/close.json */
+    /** /admin/orders/450789469/open.json */
+    /** /admin/orders/450789469/cancel.json */
+
+    /**
+     * POST /admin/orders.json
+     * Creates a new order.
+     *
+     * @test
+     * @throws \Dan\Shopify\Exceptions\InvalidOrMissingEndpointException
+     * @throws \ReflectionException
+     */
+    public function it_creates_a_new_order()
+    {
+        $api = \Dan\Shopify\Shopify::fake([
+            TransactionMock::create(OrderFactory::create(), 201)
+        ]);
+
+        $response = $api->orders->post(json_decode('{
+            "line_items": [
+              {
+                "variant_id": 447654529,
+                "quantity": 1
+              }
+            ]
+          }', true));
+
+        $this->assertEquals(201, $api->lastResponseStatusCode());
+        $this->assertTrue(is_array($response));
+        $this->assertEquals('POST', $api->lastRequestMethod());
+        $this->assertEquals('/admin/orders.json', $api->lastRequestUri());
     }
 }
