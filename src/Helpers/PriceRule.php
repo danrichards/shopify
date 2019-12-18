cc7c3fd4f47d3f8c395062f480e545dfe59a50bb
Hiram Cruz <hiram@shineon.com>
2019-12-18 09:52:45 -0500

    add code to handle discount_codes from price_rule

--- src/Helpers/PriceRule.php
+++ src/Helpers/PriceRule.php
@@ -4,7 +4,31 @@
 namespace Dan\Shopify\Helpers;
 
 
+use Dan\Shopify\Exceptions\InvalidOrMissingEndpointException;
+
 class PriceRule extends Endpoint
 {
+    /**
+     * @param string $endpoint
+     * @return $this|Endpoint
+     * @throws InvalidOrMissingEndpointException
+     */
+    public function __get($endpoint)
+    {
+        if ($endpoint == 'discount_codes') {
+            $client = $this->client;
 
+            if (empty($client->ids)) {
+                throw new InvalidOrMissingEndpointException('The discount_codes endpoint on price_rules requires a price_rule ID. e.g. $api->price_rules(123)->discount_codes->get()');
+            }
+
+            $client->queue[] = [$client->api, $client->ids[0] ?? null];
+            $client->api = 'discount_codes';
+            $client->ids = [];
+
+            return $this;
+        } else {
+            return parent::__get($endpoint);
+        }
+    }
 }
